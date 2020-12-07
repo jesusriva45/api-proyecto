0a538f6a92fbf6b341d868a83d0b0062d63721c5
jesusriva45 <jesusriva45@gmail.com>
2020-12-07 11:47:17 -0500

    Commit Consultas

--- src/main/java/com/colegio/service/SistemaEvaluacionImpl.java
+++ src/main/java/com/colegio/service/SistemaEvaluacionImpl.java
@@ -3,11 +3,12 @@ package com.colegio.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.stereotype.Service;
 
 import com.colegio.entity.SistemaEvaluacion;
 import com.colegio.repository.SistemaEvaluacionRepository;
 
+@Service
 public class SistemaEvaluacionImpl implements SistemaEvaluacionService{
 	
 	
